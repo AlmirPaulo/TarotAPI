65538b7d17b99d0596582e23ec742b619f14731e
AlmirPaulo <ap.freelas@gmail.com>
2021-06-30 20:01:48 -0300

    more data

--- README.md
+++ README.md
@@ -5,15 +5,35 @@ A RESTFul API to read tarot cards made in Python with Flask.
 
 <!--## Endpoints
 
+**Root endpoint:** Show all cards data. 
+
+    .../api/
+
+**Read endpoint:** Draft the determined number of cards randomly.
+
+    .../api/read/<number>
+
+**Card endpoint:** Show the chosen card (by the id. Which, in the case of Major Arcana, matches the card number).
+
+    .../api/card/<id> 
+
 ## Technologies
 
 * Python
 * Flask
-* Flask RESTFul
 * Gunicorn
 * Nginx
 
 ## For Bug Reports
 
 Please open an Issue.
+
+## Future Plans
+
+* Include the Minor Arcana
+* Include other Images from other tarot decks
+
+## Contribute
+If this project have helped you in some way to earn some money and you're wondering how to say "thank you", consider to buy me coffee! I would appreciate very much!
+ 
 
--- app.py
+++ app.py
@@ -6,27 +6,29 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(module)s:%(leveln
 
 app = Flask(__name__)
 api = Blueprint('api', __name__)
+app.register_blueprint(api, url_prefix='/api')
 
-
-@app.route('/')
+@api.route('/')
 def ping():
     return cards
 
-@app.route('/read/<int:draft>')
-def draft(draft):
+@api.route('/read/<int:draft>')
+def read(draft):
+    if draft > 21:
+        return 'Sorry, not enough cards...'
     pick = random.choices(cards['cards'], k=draft)
     final = jsonify(pick)
     return final
     
-@app.route('/card/<int:id>') 
+@api.route('/card/<int:id>') 
 def card(id):
     for card in cards['cards']:
         if card['id'] == id:
             return card
+    return 'Card not found.'
 
 
 
 
 if __name__ == '__main__':
-    app.register_blueprint(api, url_prefix='/api')
     app.run(debug=True)
