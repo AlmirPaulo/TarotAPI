f6f613aee8b610fdc23d5a6f09db04bbc1ba8d6d
AlmirPaulo <ap.freelas@gmail.com>
2021-09-17 14:50:26 -0300

    update

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 
 A RESTFul API to read tarot cards made in Python with FastAPI. 
 
-The API can be accessed at:  
+The API can be accessed at: 
     
-     https://TarotAPI.almirpaulo.repl.co/v1
+     https://TarotAPI.almirpaulo.repl.co/
 
 ## Documentation
 
@@ -28,7 +28,7 @@ Please open an Issue.
 * Include other Images from other tarot decks like Marseilles, etc. 
 
 ## Contribute
-If this project have helped you in some way to earn some money and you're wondering how to say "thank you", consider to "buy me coffee"! I would appreciate very much!
+If this project have helped you in some way to earn some money, consider to "buy me coffee"! I would appreciate very much!
  
 [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/C0C26878E)
 
--- api.py
+++ api.py
@@ -4,43 +4,46 @@ from deck import cards
 import uvicorn, random, logging
 
 
-#logging.basicConfig(level=logging.INFO, filename='server.log',format='%(asctime)s:%(module)s:%(levelname)s:%(message)s')
+# logging.basicConfig(level=logging.INFO, filename='server.log',format='%(asctime)s:%(module)s:%(levelname)s:%(message)s')
 
-v1 = FastAPI()
+api = FastAPI()
 
-v1.add_middleware(
+api.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],)
+    allow_origins=["*"],
+)
 
-@v1.get('/v1')
+
+@api.get("/")
 def full_deck():
-    '''
-    Show all cards data. 
+    """
+    Show all cards data.
 
-    '''
-    logging.info('root')
+    """
+    logging.info("root")
     return cards
 
 
-@v1.get('/v1/read_maj/{draft}')
-def read_maj(draft:int):
-    '''
+@api.get("/read_maj/{draft}")
+def read_maj(draft: int):
+    """
     Draft the determined number of cards randomly, considering only the major arcana.
-    '''
+    """
     if draft > 22:
         return "Sorry, not cards enough."
     draft_cards = []
     while draft > 0:
         pick = random.randint(0, 21)
-        for card in cards['cards']:
-            if card['id'] == pick and card not in draft_cards:
+        for card in cards["cards"]:
+            if card["id"] == pick and card not in draft_cards:
                 draft_cards.append(card)
                 draft = draft - 1
-            
-    logging.info('read_maj')
+
+    logging.info("read_maj")
     return draft_cards
 
-# @v1.get('/v1/read_min/{draft}')
+
+# @api.get('/api/read_min/{draft}')
 # def read_min(draft:int):
 #     if draft > 56:
 #         return "Sorry, not cards enough."
@@ -51,36 +54,35 @@ def read_maj(draft:int):
 #             if card['id'] == pick and card not in draft_cards:
 #                 draft_cards.append(card)
 #                 draft = draft - 1
-            
+
 #     return draft_cards
 
-@v1.get('/v1/read_whole/{draft}')
-def read_whole(draft:int):
-    '''
+
+@api.get("/read_whole/{draft}")
+def read_whole(draft: int):
+    """
     Draft the determined number of cards randomly, considering the whole deck.
-    '''
+    """
     if draft > 22:
-        return 'Sorry, not enough cards...'
-    pick = random.choices(cards['cards'], k=draft)
-    logging.info('read_whole')
+        return "Sorry, not enough cards..."
+    pick = random.choices(cards["cards"], k=draft)
+    logging.info("read_whole")
     return pick
-    
-@v1.get('/v1/card/{id}') 
-def card(id:int):
-    '''
+
+
+@api.get("/card/{id}")
+def card(id: int):
+    """
     Show the selected card data (by id).
-    '''
-    for card in cards['cards']:
-        if card['id'] == id:
-            logging.info('card')
+    """
+    for card in cards["cards"]:
+        if card["id"] == id:
+            logging.info("card")
             return card
-        
-    logging.info('card')
-    return 'Card not found.'
+
+    logging.info("card")
+    return "Card not found."
 
 
-
-
-if __name__ == '__main__':
-    uvicorn.run("api:v1", reload=True)
-    
+if __name__ == "__main__":
+    uvicorn.run("api:api", reload=True)
